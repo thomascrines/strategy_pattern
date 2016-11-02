768d302c19edd855997775f667ddb4abd369d7fa
Thomas Crines <thomascrines@hotmail.com>
2016-11-02 20:58:49 +0000

    working copy

--- NormalForm.java
+++ NormalForm.java
@@ -1,5 +1,5 @@
 public class NormalForm implements Strategy {
-  public String defineAppetite(int date){
+  public void defineAppetite(int date){
     System.out.println("I eat normal things like vegetables");
     }
   }
